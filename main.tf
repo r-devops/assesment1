647412f67e681d78d9627ce2083022c7d3422037
Raghu K <raghuk.vit@gmail.com>
2023-07-12 19:05:20 +0530

    Create Minikube

--- main.tf
+++ main.tf
@@ -16,4 +16,25 @@ module "vpc" {
   }
 }
 
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
 
+  aws_region    = "us-east-1"
+  cluster_name  = "minikube"
+  aws_instance_type = "t3.large"
+  ssh_public_key = "~/.ssh/id_rsa.pub"
+  aws_subnet_id = module.vpc.private_subnets[0]
+  hosted_zone = "rdevopsb73.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
